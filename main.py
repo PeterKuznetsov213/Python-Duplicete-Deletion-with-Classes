8f28e117127f615c117b166e0aa88b8611cb4a0f
PeterKuznetsov213 <peterkuznetsov213@gmail.com>
2022-11-23 00:40:55 +0400

    all 3 classes made

--- main.py
+++ main.py
@@ -2,28 +2,6 @@ from abc import ABC, abstractmethod
 from typing import List
 
 
-# m = ["a", "z", "a", "b", "b", "a", "z", "a", "a", "e", "z", "a"]
-
-
-# def delete_double(m: List[str]) -> List:
-#     indexes_ignore = check_double(m)
-#
-#     if indexes_ignore:
-#         new_array = [x for i, x in enumerate(m) if i not in indexes_ignore]
-#         return delete_double(new_array)
-#     return m
-
-
-# def check_double(m: List[str]) -> List[int]:
-#     counter = len(m) - 1
-#     result = []
-#     for i in range(counter):
-#         if m[i] == m[i + 1]:
-#             result += [i, i + 1]
-#     return result
-#
-# print(delete_double(m))
-
 class Double(ABC):
     def __init__(self, massive: List[str]):
         self.massive = massive
@@ -37,7 +15,7 @@ class Double(ABC):
         pass
 
     def get_massive(self):
-        print(self.massive)
+        return self.massive
 
 
 class DoubleNearby(Double):
@@ -56,39 +34,44 @@ class DoubleNearby(Double):
         indexes_ignore = DoubleNearby.checking_double(self)
         if indexes_ignore:
             new_array = [x for i, x in enumerate(self.massive) if i not in indexes_ignore]
-            self.massive=new_array
+            self.massive = new_array
             return DoubleNearby.deleting_double(self)
         return self.massive
 
 
-message = DoubleNearby(["a", "z", "a", "b", "b", "a", "z", "a", "a", "e", "z", "a"])
-# print(message.checking_double())
-print(message.deleting_double())
-# class Dog(ABC):
-#     def __init__(self, name: str, age: int):
-#         self.age = age
-#         self.name = name
-#
-#
-#     def get_name(self):
-#         print(self.name)
-#
-#     @abstractmethod
-#     def eat(self):
-#         pass
-#
-#
-# class Taksa(Dog):
-#
-#     def __init__(self, name: str, age: int):
-#         super().__init__(name, age)
-#
-#     def eat(self):
-#         return "я ем"
-#
-#
-#
-# dog = Taksa("dagy", 22)
-# print(dog.get_name())
-# dog.weight = 15
-# print(dog.weight, dog.eat())
+class DoubleOnTheSides(Double):
+    def __init__(self, massive: List[str]):
+        super().__init__(massive)
+
+    def checking_double(self):
+        return self.massive[0] == self.massive[-1]
+
+    def deleting_double(self):
+        if DoubleOnTheSides.checking_double(self):
+            self.massive = self.massive[1:-1]
+            DoubleOnTheSides.deleting_double(self)
+        return self.massive
+
+
+class DoubleSet(Double):
+    def __init__(self, massive: List[str]):
+        super().__init__(massive)
+
+    def checking_double(self):
+        pass
+
+    def deleting_double(self):
+        self.massive = set(self.massive)
+        return self.massive
+
+
+message1 = DoubleNearby(["a", "b", "b", "a", "z", "a", "a", "e", "z", "b", "a"])
+message2 = DoubleOnTheSides(["a", "b", "b", "a", "z", "a", "a", "e", "z", "b", "a"])
+message3 = DoubleSet(["a", "b", "b", "a", "z", "a", "a", "e", "z", "b", "a", "x"])
+
+print(f"Массив #1 - {message1.get_massive()}")
+print(f"Удаление дубликатов стоящих рядом в массиве - {message1.deleting_double()}\n")
+print(f"Массив #2 - {message2.get_massive()}")
+print(f"Удаление дубликатов стоящих по краям массива - {message2.deleting_double()}\n")
+print(f"Массив #3 - {message3.get_massive()}")
+print(f"Удаление не уникальных значений - {message3.deleting_double()}")
